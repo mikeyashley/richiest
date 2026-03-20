14843a1c543b984d749a02b304d079b6c57c9edb
mikeyashley <92893809+mikeyashley@users.noreply.github.com>
2026-03-19 21:00:34 -0400

    baseline

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', () => {
+    return gulp.src('src/styles/*.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('dist/styles'));
+});
+gulp minify - css
